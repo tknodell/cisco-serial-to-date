f9b911dd6a4bb5ede545cc19326632aff80c6e7d
tknodell <tylerknodell@gmail.com>
2019-10-28 21:26:19 -0600

    mfg date tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_serial_getLocation(t *testing.T) {
@@ -52,6 +53,37 @@ func Test_newSerial(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("newSerial() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_serial_getMfgDate(t *testing.T) {
+	type fields struct {
+		serialNum string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    time.Time
+		wantErr bool
+	}{
+		{"valid", fields{serialNum: "FAA04459FNI"}, time.Date(2000, time.November, 06, 00, 0, 0, 0, time.UTC), false},
+		{"valid", fields{serialNum: "F091937V497"}, time.Date(2015, time.September, 07, 00, 0, 0, 0, time.UTC), false},
+		{"badYear", fields{serialNum: "FAA04609FNI"}, time.Time{}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &serial{
+				serialNum: tt.fields.serialNum,
+			}
+			got, err := s.getMfgDate()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("serial.getMfgDate() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("serial.getMfgDate() = %v, want %v", got, tt.want)
 			}
 		})
 	}

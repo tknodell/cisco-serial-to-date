3b46abc543b0afc784e790a4ab340f51ddd8cd91
tknodell <tylerknodell@gmail.com>
2019-10-29 16:36:41 -0600

    get rid of fields we dont need

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 
 rm -rf ./bin
 mkdir -p ./bin
+go test .
 go build -o ./bin/serial_to_date main.go
 echo "Built succesfully, run ./bin/serial_to_date"
--- main_test.go
+++ main_test.go
@@ -7,22 +7,19 @@ import (
 )
 
 func Test_serial_getLocation(t *testing.T) {
-	type fields struct {
-		serialNum string
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		want   string
+		name      string
+		serialNum string
+		want      string
 	}{
-		{"China", fields{"FOC"}, "Foxconn - Shenzhen China"},
-		{"Texas", fields{"TAU"}, "Solectron - Texas"},
-		{"Invalid", fields{"foo"}, "Unknown"},
+		{"China", "FOC0000xxxx", "Foxconn - Shenzhen China"},
+		{"Texas", "TAU0000xxxx", "Solectron - Texas"},
+		{"Invalid", "foo0000xxxx", "Unknown"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &serial{
-				serialNum: tt.fields.serialNum,
+				serialNum: tt.serialNum,
 			}
 			if got := s.getLocation(); got != tt.want {
 				t.Errorf("serial.getLocation() = %v, want %v", got, tt.want)
@@ -33,20 +30,19 @@ func Test_serial_getLocation(t *testing.T) {
 
 func Test_newSerial(t *testing.T) {
 	type args struct {
-		serialnum string
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    *serial
-		wantErr bool
+		name      string
+		serialnum string
+		want      *serial
+		wantErr   bool
 	}{
-		{"valid", args{serialnum: "FAA04459FNI"}, &serial{serialNum: "FAA04459FNI"}, false},
-		{"invalid", args{serialnum: "foobar"}, nil, true},
+		{"valid", "FAA04459FNI", &serial{serialNum: "FAA04459FNI"}, false},
+		{"invalid", "foobar", nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := newSerial(tt.args.serialnum)
+			got, err := newSerial(tt.serialnum)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("newSerial() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -59,25 +55,22 @@ func Test_newSerial(t *testing.T) {
 }
 
 func Test_serial_getMfgDate(t *testing.T) {
-	type fields struct {
-		serialNum string
-	}
 	tests := []struct {
-		name    string
-		fields  fields
-		want    time.Time
-		wantErr bool
+		name      string
+		serialNum string
+		want      time.Time
+		wantErr   bool
 	}{
-		{"valid", fields{serialNum: "FAA04459FNI"}, time.Date(2000, time.November, 06, 00, 0, 0, 0, time.UTC), false},
-		{"valid", fields{serialNum: "F091937V497"}, time.Date(2015, time.September, 07, 00, 0, 0, 0, time.UTC), false},
-		{"valid", fields{serialNum: "FAA10129FBJ"}, time.Date(2006, time.March, 20, 00, 0, 0, 0, time.UTC), false},
+		{"valid", "FAA04459FNI", time.Date(2000, time.November, 06, 00, 0, 0, 0, time.UTC), false},
+		{"valid", "F091937V497", time.Date(2015, time.September, 07, 00, 0, 0, 0, time.UTC), false},
+		{"valid", "FAA10129FBJ", time.Date(2006, time.March, 20, 00, 0, 0, 0, time.UTC), false},
 
-		{"badWeek", fields{serialNum: "FAA04609FNI"}, time.Time{}, true},
+		{"badWeek", "FAA04609FNI", time.Time{}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &serial{
-				serialNum: tt.fields.serialNum,
+				serialNum: tt.serialNum,
 			}
 			got, err := s.getMfgDate()
 			if (err != nil) != tt.wantErr {
